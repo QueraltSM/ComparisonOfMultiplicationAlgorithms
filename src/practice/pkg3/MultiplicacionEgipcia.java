d68aa624623ebcbef5e4184615dbece0ffb2d237
QueraltSM <queraltsosa@hotmail.com>
2017-12-07 19:06:54 +0000

    Update MultiplicacionEgipcia.java

--- src/practice/pkg3/MultiplicacionEgipcia.java
+++ src/practice/pkg3/MultiplicacionEgipcia.java
@@ -2,7 +2,6 @@ package practice.pkg3;
 import java.math.BigInteger;
 
 public class MultiplicacionEgipcia implements Multiplicacion {
-
     private final BigInteger DOS = new BigInteger("2");
     private BigInteger[] resultado;
 
@@ -10,11 +9,9 @@ public class MultiplicacionEgipcia implements Multiplicacion {
     public void multiplica(BigInteger n, BigInteger m, DatosEstadisticos datos) {
         datos.setOrden(m);
         resultado = new BigInteger[]{new BigInteger("0"), new BigInteger("0")};
-        long inicio = System.currentTimeMillis();
-
+        long inicio = System.nanoTime();
         recursivaEgipcia(n, m, new BigInteger("1"));
-
-        datos.setTiempo((float) ((System.currentTimeMillis() - inicio) / 1000.0));
+        datos.setTiempo((float) ((System.nanoTime() - inicio) / 1000000.));
         datos.setResultado(resultado[0]);
     }
 
