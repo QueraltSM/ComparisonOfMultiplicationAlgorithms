b7ef6d14aed1e9614e4eafc78ec41b3e13fafd49
QueraltSM <queraltsosa@hotmail.com>
2017-12-07 19:05:01 +0000

    Update SumasSucesivas.java

--- src/practice/pkg3/SumasSucesivas.java
+++ src/practice/pkg3/SumasSucesivas.java
@@ -2,23 +2,21 @@ package practice.pkg3;
 import java.math.BigInteger;
 
 public class SumasSucesivas implements Multiplicacion {
-
     private final BigInteger UNO = new BigInteger("1");
     private final BigInteger CERO = new BigInteger("0");
 
     @Override
     public void multiplica(BigInteger n, BigInteger m, DatosEstadisticos datos) {
         datos.setOrden(m);
-        long inicio = System.currentTimeMillis();
+        long inicio = System.nanoTime();
         datos.setResultado(recursivaSumas(n, m));
-        datos.setTiempo((System.currentTimeMillis() - inicio) / 1000);
+        datos.setTiempo((float) ((System.nanoTime() - inicio) / 1000000.));
     }
 
     private BigInteger recursivaSumas(BigInteger n, BigInteger m) {
         if (m.compareTo(CERO) == 0) {
             return CERO;
         }
-
         return (n.add(recursivaSumas(n, m.subtract(UNO))));
     }
 
@@ -26,5 +24,4 @@ public class SumasSucesivas implements Multiplicacion {
     public String nombreMetodo() {
         return "Sumas Sucesivas";
     }
-
 }
